374a8231a28e86d0a2bdf5b35d5fde2e231451a6
Kushal CG <kushalganesh2@gmail.com>
2022-03-31 17:47:24 +0530

    AuthComplete

--- Authentication/authDemo/index.js
+++ Authentication/authDemo/index.js
@@ -1,16 +1,73 @@
 const express = require("express")
 const app = express()
 const User = require("./user")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const { hash } = require("bcrypt")
+const session = require("express-session")
+
+mongoose.connect("mongodb://localhost:27017/authDemo", {useNewUrlParser:true, useUnifiedTopology:true})
+    .then(() => {
+        console.log("Connection Open!")
+    })
+    .catch(err => {
+        console.log("OH no error")
+        console.log(err)
+    })
 
 app.set("view engine", "ejs")
 app.set("views", "views")
 
-app.get("/secret", (req, res) => {
-    res.send("You cannot see me")
+app.use(express.urlencoded({extended:true}))
+app.use(session({secret:"ggs"}))
+
+const requireLogin = (req, res, next) => {
+    if(!req.session.user_id){
+        return res.redirect("/login")
+    }
+    next()
+}
+
+app.post("/register", async(req, res) => {
+    const {password, username} = req.body
+    const user = new User({username,password})
+    await user.save()
+    console.log(user.password)
+    req.session.user_id = user.id
+    res.redirect("/")
+})
+
+app.post("/login", async(req, res) => {
+    const {password, username} = req.body
+    const user = User.findAndValidate(username, password)
+    if(user){
+        req.session.user_id = user.id
+        res.render("secret")
+    }
+    else{
+        res.send("Try again")
+    }
+})
+
+app.get("/", (req, res) => {
+    res.send("This is home page")
+})
+
+app.get("/secret",  requireLogin,(req, res) => {
+    res.send("This cannot be accessed without login")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
 })
 
 app.get("/register", (req, res) => {
     res.render("register")
+})
+
+app.post("/logout", (req, res) => {
+    req.session.user_id = null
+    res.redirect("/login")
 })
 
 app.listen(3000, () => {
--- Authentication/authDemo/user.js
+++ Authentication/authDemo/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt")
 const mongoose =  require("mongoose")
 const userSchema = new mongoose.Schema({
     username:{
@@ -8,6 +9,17 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:[true, "Password is required"]
     }
+})
+
+userSchema.statics.findAndValidate = async function(username, password){
+    const user = await this.findOne({username})
+    const isValid = await bcrypt.compare(password, user.password)
+    return isValid?user:false
+}
+
+userSchema.pre("save",async function(next){
+    this.password = await bcrypt.hash(this.password, 12)
+    next()
 })
 
 module.exports = mongoose.model("user", userSchema)
